23650a733f8d9aa8d51895498ee8f8bdcc5048b4
Elizer Ponio Jr <elizerponiojr@gmail.com>
2023-01-19 22:04:34 +0800

    Create assignment2.java

---
+++ assignments/assignment2.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class App {
+
+    static Scanner scan = new Scanner(System.in);
+    static final int targetWeight = 150;
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("Enter current weight in lbs:");
+        int weight = scan.nextInt();
+
+        if (weight > 150) {
+            loseWeight(weight);
+        } else {
+            System.out.println("Current weight must be greater than 150 lbs!");
+        }
+        scan.close();
+    }
+
+    static void loseWeight(int currentWeight) {
+
+        System.out.println("Eating right!");
+        System.out.println("Doing exercises!");
+
+        currentWeight = checkWeight(currentWeight);
+
+        // Define the base case
+        // Do something to reach the target
+        // Do the recursive call
+      
+        // if the target weight has been reached, print the code below.
+        // System.out.println("I have reached my target weight!");
+           
+    }
+
+    static int checkWeight(int weight) {
+        System.out.println("My current weight is: " + weight + "lbs");
+        return weight;
+    }
+
+    static int fixDietandExercise(int weight) {
+
+        System.out.println("Fixing Diet and Exercise!");
+        System.out.println("Enter weight lost in lbs!");
+        int weightlost = scan.nextInt();
+
+        int currentWeight = weight - weightlost;
+        return currentWeight;
+    }
+}
