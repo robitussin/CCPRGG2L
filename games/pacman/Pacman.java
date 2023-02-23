e9ad28e3c5fed0c4c5eca95d946cd527e56cb623
robitussin <elizerponiojr@gmail.com>
2023-02-23 17:50:59 +0800

    Added pacman

---
+++ games/pacman/Pacman.java
@@ -0,0 +1,30 @@
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setTitle("Pacman");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 420);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            Pacman ex = new Pacman();
+            ex.setVisible(true);
+        });
+    }
+}
